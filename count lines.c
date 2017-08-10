acd80394a37f112da011c6d972788a8b14ec75ec
Greyzerus <wetal777vk@yandex.ru>
2017-08-10 14:30:05 +0400

    Add 'count lines.c'.

---
+++ count lines.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[]) {
+    if (argc == 1) {
+        puts ("Error: enter filename(s) as argument(s)\n");
+        return 1;   
+    }
+    int total=0;
+    for (int i=1; i<argc; i++) {
+        FILE *iFile;
+        iFile = fopen (argv[i], "r");
+        if (!iFile) {
+            printf ("Error: %s: Can't open file\n", argv[i]);
+            continue;
+        }
+        int Lines = 0;
+        char buf = 'c';
+        do {
+            buf = fgetc (iFile);
+            if (buf == '\n')
+                ++Lines;
+        }
+        while (buf != EOF);
+        printf ("%d %s\n", Lines, argv[i]);
+        total+=Lines;
+    }
+    if (argc>2)
+        printf ("%d total\n", total);
+    return 0;    	
+}
